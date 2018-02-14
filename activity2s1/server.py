462ebe50e9c3a310a4c6f3d514068357267e1516
Enkbaatar Battulga <ebar1126@gmail.com>
2018-02-13 23:18:59 -0500

    added changes for class activity

--- activity2s1/server.py
+++ activity2s1/server.py
@@ -1,1 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", name="Alex")
+
+@app.route("/process", methods=["POST"])
+def process():
+    name = request.form["myName"]
+    location = request.form["location"]
+    gender = request.form["gender"]
+    return render_template("formdata.html", name=name, location=location, gender=gender)
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html", hobbies = ["Coding", "Reading", "Traveling"])
+app.run(debug=True)
