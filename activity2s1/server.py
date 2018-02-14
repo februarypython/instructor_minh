d521c3d7cbce440ad681348687c458d758ee174b
Cindy Kalkomey <cindy@lakesong.net>
2018-02-13 22:30:32 -0600

    added changes for class activity

--- activity2s1/server.py
+++ activity2s1/server.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+@app.route('/') #our index.html page will handle rendering our form
+def index():
+    blogger_name="Cindy Kalkomey"
+    blogger_photo="images/grp_sunset.jpg"
+    return render_template('index.html', name=blogger_name, photo=blogger_photo)
+
+@app.route('/process', methods=['POST'])  
+def process():
+    name = request.form["name"]
+    location = request.form["location"]
+    gender = request.form["gender"]    
+    return render_template('process.html', name=name, location=location, gender=gender)
+
+@app.route('/hobbies')
+def hobbies():
+    hobbies = ["Sports", "cooking", "coding"]
+    return render_template('hobbies.html', hobbies=hobbies)
+
+app.run(debug=True) 
