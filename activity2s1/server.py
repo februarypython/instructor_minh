56ec9d730d253017e1c1b3bf3f54adf19c233501
minh <nguyenhminhp@gmail.com>
2018-02-13 19:03:56 -0800

    finish activty 2s1

--- activity2s1/server.py
+++ activity2s1/server.py
@@ -1,1 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", name="Minh")
+
+@app.route("/process", methods=["POST"])
+def process():
+    gender = request.form["gender"]
+    name = request.form["first_name"]
+    location = request.form["location"]
+    return render_template("formdata.html", name=name, gender=gender, location=location)
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html", hobbies = ["Sports", "cooking", "coding"])
+app.run(debug=True)
