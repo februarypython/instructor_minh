38c9709d71b9c61f8b179fbe17ae867908d5dd83
minh <nguyenhminhp@gmail.com>
2018-02-15 17:05:34 -0800

    completed 2s2 activity

--- activity2s2fakeLoginReg/server.py
+++ activity2s2fakeLoginReg/server.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, redirect, session, request, flash
 app = Flask(__name__)
 app.secret_key = 'My super secret app' # you need to set a secret key for security purposes
 
-@app.route('/')
+@app.route('/') #
 def index():
+  if "users" in session:
+    print session["users"]
+  if "email" in session:
+    return redirect("/home")
   return render_template("index.html")
+
+@app.route("/process_info/<action>", methods=["POST"])
+def process(action):
+  if "users" not in session:
+    session["users"] = []
+  if action == "register":
+    session["users"].append(request.form)
+    flash("Thanks for registering, please login")
+    return redirect("/")
+  elif action == "login":
+    for user in session["users"]:
+      if user["email"] == request.form["email"] and user["password"] == request.form["password"]:
+        session["email"] = user["email"]
+        return redirect("/home")
+  else:
+    return redirect("/")
 
 app.run(debug=True)
