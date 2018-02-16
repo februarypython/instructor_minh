c331fe22116d13cc97ef5c34295bc4b6cd01e9b3
minh <nguyenhminhp@gmail.com>
2018-02-15 19:06:16 -0800

    finished activity

--- activity2s2fakeLoginReg/server.py
+++ activity2s2fakeLoginReg/server.py
@@ -4,17 +4,25 @@ app.secret_key = 'My super secret app' # you need to set a secret key for securi
 
 @app.route('/') #
 def index():
+  #print users just to see
   if "users" in session:
     print session["users"]
   if "email" in session:
     return redirect("/home")
   return render_template("index.html")
 
+
 @app.route("/process_form/<action>", methods=["POST"])
 def process(action):
   if "users" not in session:
     session["users"] = []
   if action == "register":
+    #check for user
+    for user in session["users"]:
+      if user["email"] == request.form["email"]:
+        flash("email already exist")
+        return redirect("/")
+    #if we reach this point, for loop finished without user["email"] == request.form["email"], we can register
     session["users"].append(request.form)
     flash("Thanks for registering, please login")
     return redirect("/")
@@ -30,4 +38,15 @@ def process(action):
   else:
     return redirect("/")
 
+@app.route("/home")
+def home():
+  #find user in "database"
+  for user in session["users"]:
+    if user["email"] == session["email"]:
+      return render_template("home.html", logged_user=user)
+
+@app.route("/clear_session")
+def clear_session():
+  session.clear()
+  return redirect("/")
 app.run(debug=True)
