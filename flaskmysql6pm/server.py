d50d551b2129116bbb85f11d58eb91a2e7fc64a8
Cindy Kalkomey <cindy@lakesong.net>
2018-02-28 23:39:10 -0600

    New feature to add and list tweets of logged in user

--- flaskmysql6pm/server.py
+++ flaskmysql6pm/server.py
@@ -33,11 +33,32 @@ def login():
         user = users[0] #the first dictionary
         if user["password"] == request.form["password"]:
             session["id"] = user["id"]
-            return "logged in"
+
+            query = "SELECT tweets AS content, DATE_FORMAT(created_at, '%m/%d/%y')AS tweet_date FROM tweets WHERE tweets.users_id="+str(session['id'])
+            results = mysql.query_db(query)
+            return render_template('dashboard.html', all_tweets=results) # pass data to our template
         else:
             flash("wrong password")
             return redirect("/")
     else:
         flash("email doesnt exist")
         return redirect("/")
+
+@app.route("/addtweet", methods=["POST"])
+def addtweet():
+    # Write users info query as a string. 
+    query = "INSERT INTO tweets ( tweets, users_id, created_at, updated_at) VALUES (:content, :loggedin_id, NOW(), NOW() )"
+    # We'll then create a dictionary of data from the POST data received.
+    data = {
+            'content': request.form['content'],
+            'loggedin_id': session['id']
+            }
+    # Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    # run query to get all of users.id tweets
+    query = "SELECT tweets AS content, DATE_FORMAT(created_at, '%m/%d/%y')AS tweet_date FROM tweets WHERE tweets.users_id="+str(session['id'])
+    results = mysql.query_db(query)
+
+    return render_template('dashboard.html', all_tweets=results) # pass data to our template
+
 app.run(debug=True)
